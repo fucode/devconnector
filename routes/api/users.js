60efa58a01c783a4102dc83911c8d5a561838cc6
Faraz <farazuddinva@gmail.com>
2019-05-06 22:43:05 -0400

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User');
+
 // @route   POST api/users/test
 // @desc    Register user
 // @access  Public
@@ -9,12 +11,39 @@ router.post('/', [
   check('name', 'Name is required').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravatar 
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+
+      res.send('User route');
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+
+
   }
-  res.json('User route');
-});
+);
 
 module.exports = router;
